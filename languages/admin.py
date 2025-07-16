bff2af6d182eedb1ccf9b6081292f0816728ecab
mairima <aissatou.mairi@gmail.com>
2025-07-16 18:00:28 +0200

    Add Django admin support for Language Model

--- languages/admin.py
+++ languages/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Language
+
+@admin.register(Language)
+class LanguageAdmin(admin.ModelAdmin):
+    list_display = ('name', 'difficulty')
+    search_fields = ('name',)
