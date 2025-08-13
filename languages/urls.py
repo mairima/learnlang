9935dbf6b6c870e14c8237ffbea7010fcac8de99
mairima <aissatou.mairi@gmail.com>
2025-08-13 23:33:41 +0200

    Update admin dashboard with bookings counts, time and dates

--- languages/urls.py
+++ languages/urls.py
@@ -1,33 +1,25 @@
 # languages/urls.py
 from django.urls import path
-from .views import (
-    home,
-    english,
-    get_tutor,
-    contact_us,
-    book_tutor,
-    my_bookings_view,
-    edit_booking_view,
-    delete_booking_view,
-    post_login_redirect,
-    student_dashboard,
-)
+from .import views # import views
 
 urlpatterns = [
-    path("", home, name="home"),
+    path("", views.home, name="home"), 
 
     # Content pages
-    path("english/", english, name="english"),
-    path("get-tutor/", get_tutor, name="tutor"),  
-    path("contact/", contact_us, name="contact_us"),
+    path("english/", views.english, name="english"),
+    path("get-tutor/", views.get_tutor, name="tutor"),
+    path("contact/", views.contact_us, name="contact_us"),
 
     # Booking
-    path("booking/", book_tutor, name="booking"),
-    path("my-bookings/", my_bookings_view, name="my_bookings"),
-    path("booking/<int:booking_id>/edit/", edit_booking_view, name="edit_booking"),
-    path("booking/<int:booking_id>/delete/", delete_booking_view, name="delete_booking"),
+    path("booking/", views.book_tutor, name="booking"),
+    path("my-bookings/", views.my_bookings_view, name="my_bookings"),
+    path("booking/<int:booking_id>/edit/", views.edit_booking_view, name="edit_booking"),
+    path("booking/<int:booking_id>/delete/", views.delete_booking_view, name="delete_booking"),
 
     # Auth routing
-    path("after-login/", post_login_redirect, name="after_login"),
-    path("dashboard/student/", student_dashboard, name="student_dashboard"),
+    path("after-login/", views.post_login_redirect, name="after_login"),
+    path("", views.home, name="home"),
+    path("dashboard/admin/", views.admin_dashboard, name="admin_dashboard"),
+    # Admin dashboard only
+    path("dashboard/admin/", views.admin_dashboard, name="admin_dashboard"),
 ]
