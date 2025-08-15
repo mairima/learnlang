c74edfade4ed7b105eaf6392548c35c682d9183a
mairima <aissatou.mairi@gmail.com>
2025-08-15 03:09:14 +0200

    Update js file for testing

--- static/js/scroll.js
+++ static/js/scroll.js
@@ -1,17 +1,20 @@
-    // Show/hide button on scroll
-    window.onscroll = function() {
-    const btn = document.getElementById("scrollToTopBtn");
+/* jshint esversion: 6 */
+/* exported scrollToTop */
+
+// Show/hide button on scroll
+window.onscroll = function () {
+    let btn = document.getElementById("scrollToTopBtn");
     if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
         btn.style.display = "block";
     } else {
         btn.style.display = "none";
     }
-    };
+};
 
-    // Scroll to top smoothly
-    function scrollToTop() {
+// Scroll to top smoothly
+function scrollToTop() {
     window.scrollTo({
         top: 0,
         behavior: 'smooth'
     });
-    }
+}
